278f6bc08752339c2c1f1d3e0af71497f3acb768
Shaun Ellis <shaun@sdellis.com>
2021-04-07 11:32:49 -0400

    adds more info to How it Works

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,20 +1,205 @@
 <template>
   <Layout>
     <h1>How It Works</h1>
-    <p>
-      With the recent retirement of Jean Michel Dumas, proprietor of Manon, we have the sudden opportunity to turn Soupçon Salon into more than just a Monday night dinner event. This summer we plan to open the doors at 19 North Main Street, for a minimum of three days per week (likely Saturday-Monday) featuring aspiring guest chefs who will fill three-month residencies. We intend to operate as a not-for-profit Beneficiary Society 501(c)(8), offering low-cost, healthy food, entertainment, products, and services to our partners, and their guests. And of course, we will offer the same Soupçon experiences that brought us together, and more. Our goals remain the same: to provide a beautiful “third place” (aka, “great good place”) and a platform for partners to share resources, build community, learn together, imagine together, and present their art and music to our community. In addition, Soupçon will focus on organizing mutual aid within the community, with our uniting mission being:
+    <p>We are approaching Soupçon Salon as a concept that may not be familiar under
+    Capitalism, in which people are pit against each other in competition. Instead of
+    creating a hierarchical system, this collective is a collaborative and
+    egalitarian experiment. We are confident it will work, but there are some core concepts and
+    values that are important for this kind of model to work. It's very important that you understand
+    these concepts.
     </p>
-    <blockquote>
-      "We unite people with the common goal of working on collective excellence for all our partners and a voluntary reciprocal exchange of resources and services for mutual benefit."
-    </blockquote>
+    <h2>Why are we doing this?</h2>
+    <blockquote>"We must create a system or be enslaved by another's." - William Blake</blockquote>
     <p>
-      As a Beneficiary Society, our success would be contingent on the number of people who become partners since only 30% of revenue of a 501(c)(8) can come from the sale of food, goods, or services. The rest must come from partnership contributions that are put into sustaining the collective. I want to be clear that these contributions are not tax deductible and while it's a non-profit, it's not a charity. In contrast to charity, mutual aid is about building "bottom-up" structures of cooperation, rather than relying on the state or wealthy philanthropists to address our community needs.
+      We are all tired of Capitalism. The pandemic has amplified its failings, as well as its
+      related structures of inequity and its colonial artifacts. But what is the best,
+      fairest alternative? We will find out. Soupçon allows us to experiment with alternative ways
+      of living and interacting. This is a social experiment that aims to prove that more
+      compassionate societies are not only possible, but preferable. This is not to say that
+      we will get everything right, and we will most certainly need to
+      adjust the dials along the way.
     </p>
     <p>
-      With that in mind, we want to make sure partnership is as inclusive as possible. We have crunched some numbers and, while nothing is set in stone yet, we could offer basic Partnerships at $5/month, and Patron Partnerships at $50/month. Both partnership levels would be extended benefits such as discounted meals, buyers' clubs, tickets to events, and use of the space for events/meetings. We would like to extend additional benefits to Patron Partners, perhaps in the form of a thank you gift, public acknowledgement, and/or in the form of a "family" membership. The point is that we have a vision and a rare opportunity, but we don't have all the details in stone yet because we need your help with that.
+      We value collaboration, autonomy, and
+      accountability. We work to uplift and center the most vulnerable among us.
+      Consistent government failings and pandemic-imposed isolation, have exacerbated the
+      divisions among us. We want to see this end. There
+      is no savior that will swoop in and make things better. We have only ourselves
+      and we embark on this journey out of love for our families, our friends, and our community.
+      Soupçon is a city-wide commune that will teach us to work together, in harmony.
+    </p>
+    <h2>What is Mutual Aid?</h2>
+    <p>
+    Luckily, we do have some promising guides as we set out to create a better society.
+    In 2020, Dean Spade published <a href="">a book on practical organizing</a> called <em>Mutual Aid: Building Solidarity
+    During This Crisis (And The Next)</em>. Spade defines "mutual aid" as collective coordination
+    to meet each other's needs, usually from an awareness that the systems we have in place
+    are not going to meet them. We see examples of mutual aid in every single social movement,
+    whether it's raising money for workers on strike, putting drinking water in the desert for
+    migrants crossing the border, or coordinating letter-writing to prisoners. These are mutual
+    aid projects. They directly meet people's survival needs, and are based on a shared
+    understanding that the conditions in which we are made to live are unjust.
     </p>
     <p>
-      If you are interested in being a part of this as a partner (at any level) as we are getting it off the ground? If the answer is yes, we are also asking for your feedback and help in guiding this project. There is no obligation at this point, but early involvement will help us get this off the ground, so please subscribe to our mailing list if you are interested in learning more and possibly being involved in supporting this effort. This will be a space for all of us to grow in together.
+    Today, many of us live in the most atomized societies in human history, which makes our
+    lives less secure and undermines our ability to organize together to change unjust
+    conditions on a larger scale. We are put into competition with each other for survival,
+    and we are forced to rely on hostile systems for the things we need. In this context, mutual aid --
+    where we choose to help each other out, share things, and put time and resources into caring
+    for the most vulnerable -- is a radical act. It's how we build solidarity and enable real
+    positive change to take root and grow.
+    </p>
+    <h2>What is a Beneficiary Society?</h2>
+    <p>A Beneficiary Society is an organization that falls under Internal Revenue Code Section 501(c)(8).
+    Most people think of not-for-profits as 501(c)(3) charitable organizations, but there
+    are many nonprofits that have different strategies for helping people. This is important distinction
+    because Soupçon Salon is not a "charitable" organization.
+    </p>
+    <p>Charity, aid, relief, and social services are terms that usually refer to
+      rich people or the government making decisions about the provision of some kind of support
+      for poor people -- that is, rich people or the government deciding who gets
+      the help, and what strings are attached. You can be sure that help like that is
+      not designed to get to the root causes of poverty and violence. It's designed to
+      help improve the image of the elites who are funding it and put a tiny, inadiquate
+      Band-Aid on the massive social wound that their greed creates. No, Soupçon is not a charity.
+    </p>
+    <p>What is it then? Soupçon Salon is a Beneficiary Society that provides a wide
+      array of benefits to its partners. Beneficiary Societies, sometimes called
+      "Fraternal Orders," began at the turn of the 20th century
+      as a way to provide benefits like insurance, money for burial services, and bulk discount purchasing to members.
+      Some examples today include The Loyal Order of Moose, The Masons, police unions, and the Boy Scouts and
+      Girl Scouts of America. However, these organizations have exclusionary
+      membership practices and very different values. In contrast, Soupçon is a non-exclusionary society, anyone is allowed to join.
+      The only requirement is that you must agree with our core principle, which is:
+      <blockquote>"We unite people with the common goal of working on collective
+        excellence for all our partners and a voluntary reciprocal exchange of
+        resources and services for mutual benefit."</blockquote>
+    </p>
+    <p>
+      We may be getting ahead of ourselves, but one notable facet of Beneficiary Societies is that
+      they operate on the "Lodge System". This means if our experiment is successful, we can
+      easily replicate it in other municipalities, and connect our partners to those who live elsewhere
+      creating solidarity and relationships among other communities beyond our own.
+    </p>
+    <h2>What am I signing up for?</h2>
+    <p>We specifically call our people in our society <em>partners,</em> not <em>members</em> for two reasons. First,
+      <em>members</em> sounds exclusive and passive. Soupçon is inclusive and expects partners to be
+      active participants in the society they want to see. In other words, partners are
+      expected to get their hands dirty once in a while. Utopia is hard work, but we can do it together.
+    </p>
+
+    <h3>What does it mean to be a Partner?</h3>
+    <p>
+      Partners pay yearly dues of $60 ($5/month) to join, and can sign up on the website or by
+      visiting the cafe. The dues are used to make sure operations go smoothly,
+      to pay the rent, and to maintain equipment and property.
+      It can also be used to fund projects and additional partner benefits.
+    </p>
+    <p>
+      Partners currently receive many benefits:
+    </p>
+      <h4>Inclusive First</h4>
+      <p>
+      Not that we see it as a "benefit," as inclusivity should simply be the way things are.
+      We design our systems of living with a commitment to including all. We support
+      and center our most vulnerable and our most oppressed members of society. We
+      support a vegan and compassionate vegetarian menu.
+      </p>
+      <h4>The Cafe</h4>
+      <p>
+      Partners are able to dine, with their guests, in the beautiful cafe. This is a chill space.
+      All are welcome. The music is local. The menu is local, vegetarian &amp; vegan, and a fraction of
+      the price for an equivalent meal elsewhere in town. The outdoor garden
+      is a relaxing sanctuary with a rustic fountain, flowers, and fresh produce for the kitchen.
+      </p>
+      <h4>Guest Chef Residency</h4>
+      <p>Soupçon aims too uplift all local aspiring artists and the culinary arts are not an exception.
+      Soupçon offers a three-month residency for emerging chefs who want to showcase their art.
+      The chefs source their
+      own menus, which are supplemented by Soupçon's relationships with local farms and CSAs.
+      The chefs take home 80% of all meal sales. Guest chefs are also encouraged to produce
+      canned, dried, or otherwise preserved food items for sale in the shop, as are general
+      partners. Chefs may be expected to provide the one day of service, perhaps in the
+      form of a public "soup kitchen," for which ingredients will be sourced by Soupçon Salon.
+      </p>
+      <h4>Support Network</h4>
+      <p>Soupçon seeks to offer direct assistance to those experiencing life difficulties.
+        In a time when many people don't have other places to turn, Soupçon will listen
+        to community needs. We will work to meet those needs and support the well-being of our community.
+      </p>
+      <h4>Event/Organizing Space</h4>
+      <p>
+      All partners have access to the space from Tuesday through Friday with a
+      requested donation of $50/night(?) and optional catering. The space can
+      fit around 35 seated patrons inside.
+      </p>
+      <h4>Artists and Craftspeople Showcases</h4>
+      <p>
+      The walls are for local art. How that will be done will be up to those who
+      get involved in shaping the gallery.
+      </p>
+      <h4>Musician/Entertainer Showcases</h4>
+      <p>
+      If you have ever performed in this space, you know there is magic. We intend to
+      hold weekly, curated events at Soupçon Salon in the model of the original experiment,
+      and we would love all the local musicians to have a chance to shine in front
+      of their neighbors. We have a PA and a space large enough for a 3-4 piece band
+      or 4 musicians if we want to reach
+      maximum capacity of 35. And of course, there is always open mic night.
+      </p>
+      <h4>Guidance and Leadership</h4>
+      <p>
+      We're all in this experiment together. We need to meet to make decisions and
+      resolve issues. We need to adapt as new partners come in with fresh ideas
+      and as conditions inevitably change. How do we do this without hierarchy?
+      </p>
+      <p>Partner meetings take place in person once a month, with an option for remote
+        participation. All partners have a say in
+        our path forward, and decisions are made via consensus. As the group
+        becomes larger, we will create teams that work on short or long-term projects
+        and report back or submit proposals to the larger group. Decisions are transparent and
+        made in accordance with the process defined by the group.
+      </p>
+      <p>
+        The first team we must create is a "Board of Directors," which is required by the IRS.
+        The Soupçon Board of Directors make operational or administrative decisions to
+        keep the organization running. In contrast to more passive and hierarchical organizations,
+        the Soupçon Board of Directors does not have the power to make certain decisions without the
+        larger group's approval. It will be up to the society to decide which
+        decisions require consensus among the larger group.
+      </p>
+    </p>
+    <h3>What is a Patron Partner?</h3>
+    <p>
+    Patron Partners are the core supporters of our organization. Without them, this would
+    not be possible. While not tax-deductible, you know that as a Patron Partner you're supporting a
+    community that is making things better in the world around us. Patron Partners
+    pay $600 per year ($50/month).
+    </p>
+    <p>
+    Patron Partners benefits:
+    <ul>
+      <li>Up to four partnerships in your "family".</li>
+      <li>Name recognition. (optional)</li>
+      <li>A "thank you" gift</li>
+    </ul>
+    </p>
+
+ <h2>How can I contribute my skills?</h2>
+  <p>We have no shortage of work to make this happen. No one partner should be
+    taking on a majority of the work. To make delegating easier, we have a living
+    to-do list (coming soon!) you can browse to see if any tasks meet your skillset.
+  </p>
+
+ <h2>Interested in Becoming a Partner?</h2>
+    <p>
+      If you are interested in being a part of this as a partner (at any level)
+      as we are getting it off the ground? If the answer is yes, we are also asking
+      for your feedback and help in guiding this project. There is no obligation at
+      this point, but early involvement will help us get this off the ground, so please
+      subscribe to our mailing list below if you are interested in learning more and possibly
+      being involved in supporting this effort. Soupçon will be a space for all of us
+      to grow in together.
     </p>
   </Layout>
 </template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,6 @@
     <g-image alt="Example image" src="~/soupcon.png" width="1000" />
 
     <h1>Soupçon Salon is back!</h1>
-
     <p>
       Soupçon Salon was a wildly successful experiment that celebrated the local community
       through food and art. Today, we are happy to announce that Soupçon is reborn as an arts-focused mutual aid collective.
@@ -17,6 +16,13 @@
       Beneficiary Society (501c8). Please read more about how it works in the <g-link to="/about/">How it Works</g-link> section. If you
       are ready to hear more, please join our mailing list below.
     </p>
+    <blockquote>"Everything worthwhile is done with others,"<br/>
+      -Moussa Kaba, Guinea independence activist and father of Miriame Kaba
+    </blockquote>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/veN9EYsV_LU"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
 
   </Layout>
 </template>
