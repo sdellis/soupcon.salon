5be80118d1511b366d3b78bea928eb6350286d8a
Shaun Ellis <shaun@sdellis.com>
2021-05-04 17:42:24 -0400

    fixes date on show page

--- src/templates/Events.vue
+++ src/templates/Events.vue
@@ -2,12 +2,12 @@
   <Layout>
     <div class="container" style="grid-column: 2 / span 1;">
       <div class="eventPage">
-        <div class="eventPage__img" 
+        <div class="eventPage__img"
             :style="{ 'background-image': 'url(' + $page.event.image + ')' }"></div>
         <div class="container container--event">
             <g-link to="/calendar/" class="back"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-left" class="svg-inline--fa fa-arrow-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path></svg>Back</g-link>
             <h1 class="eventPage__title">{{$page.event.title}}</h1>
-            <p>{{$page.event.date}}</p>
+            <p>{{niceDate}}</p>
             <div v-html="$page.event.content"></div>
         </div>
       </div>
@@ -18,6 +18,7 @@
 query ($path: String!) {
   event: events (path: $path) {
     title
+    date
     content
     author
     image
@@ -25,8 +26,14 @@ query ($path: String!) {
 }
 </page-query>
 <script>
+import dayjs from "dayjs";
 export default {
   components: {},
+  computed: {
+    niceDate() {
+      return dayjs(this.$page.event.date).format("dddd, MMMM D, YYYY h:mm A")
+    },
+  },
   metaInfo() {
     return {
       title: this.$page.event.title
